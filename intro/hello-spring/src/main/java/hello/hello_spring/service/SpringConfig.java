83b5c54148db38c53b25dc89188a1b7ff3c864b7
syjdjr <syjdjr@naver.com>
2024-07-14 20:02:22 +0900

    Revert "스프링 DB 접근 기술"
    This reverts commit 1059d837fb34407b9570671f89756862a59f2d4d.

--- intro/hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
+++ intro/hello-spring/src/main/java/hello/hello_spring/service/SpringConfig.java
@@ -1,21 +1,12 @@
 package hello.hello_spring.service;
 
-import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -23,8 +14,6 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-//        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-
+        return new MemoryMemberRepository();
     }
 }
