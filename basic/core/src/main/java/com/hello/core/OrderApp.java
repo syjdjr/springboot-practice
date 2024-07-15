0c8579d588b381a615fb958f674a5ae489df1188
syjdjr <syjdjr@naver.com>
2024-07-15 14:52:05 +0900

    feat: 스프링 예제 만들기

---
+++ basic/core/src/main/java/com/hello/core/OrderApp.java
@@ -0,0 +1,24 @@
+package com.hello.core;
+
+import com.hello.core.member.Grade;
+import com.hello.core.member.Member;
+import com.hello.core.member.MemberService;
+import com.hello.core.member.MemberServiceImpl;
+import com.hello.core.order.Order;
+import com.hello.core.order.OrderService;
+import com.hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L; // member 저장해야하기 때문이다.
+        Member member = new Member(memberId, "member4",Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        System.out.println("order = " + order);
+    }
+}
